d9b26ceffbe638ab97213445f10df2483b3407c2
Ilya <kozlov-ilya@ispras.ru>
2022-06-08 18:49:00 +0300

    rename sourse dir

--- tests/test_style.py
+++ tests/test_style.py
@@ -7,7 +7,7 @@ from style_test import StyleTest
 class TestStyle(StyleTest):
     def get_files(self) -> List[str]:
         test_dir = os.path.dirname(__file__)
-        source_dir = os.path.join(test_dir, "..", "src")
+        source_dir = os.path.join(test_dir, "..", "podrostoc")
         result = []
         for directory in [test_dir, source_dir]:
             for root, _, files in os.walk(directory):
